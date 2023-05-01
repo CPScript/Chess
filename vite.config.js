8ec30d06c95e59da3f6ef425f73cc712a68c5cce
Chai <83523587+CPScript@users.noreply.github.com>
2023-05-01 10:23:24 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
-/**
- * https://vitejs.dev/config/
- * @type { import('vite').UserConfig }
- */
+
 export default {
   plugins: [reactRefresh()],
   server: {
